d1c955ea733ae0b76c7d0265c0ff606c002cab3c
aiminnano <gabriellesmateo@gmail.com>
2026-06-05 02:49:33 +0800

    updated the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,63 @@
 import hamburger from '../../images/icon-hamburger.svg'
 import heroone from '../../images/mobile-image-hero-1.jpg'
 import close from '../../images/icon-close.svg'
+import arrow from '../../images/icon-arrow.svg'
+import arrowleft from '../../images/icon-angle-left.svg'
+import arrowright from '../../images/icon-angle-right.svg'
+
 import { useState } from 'react'
 
 function Header(){
 
     const [isOpen, setIsOpen] = useState(false);
+    const [count, setCount] = useState(1);
+
+    function previous() {
+        setCount(currCount => currCount - 1)
+    }
 
     return(
         <>
             <div className="grid place-items-center font-spartan">
-                <div className="w-full max-w-[1440px] flex">
-                    <div className={`${ isOpen ? 'block' : 'hidden'} fixed z-1 w-full h-full bg-blacklowop`}></div>
-                    <img src={heroone} className='absolute' alt="hero-1" />
-                    <div className={`${ isOpen ? 'hidden' : 'flex' } relative z-1 flex items-center justify-center w-full text-white text-[30px] p-5 pt-7`}>
-                        <div>room</div>
-                        <img src={hamburger} className='absolute left-5' onClick={() => setIsOpen(!isOpen)} alt="hamburger-icon" />
-                    </div>
-                    <div className={`${ isOpen ? 'flex' : 'hidden' } relative z-2 flex items-center justify-end bg-white p-6 pt-10 pb-10 w-full gap-8`}>
-                        <img src={close} onClick={() => setIsOpen(!isOpen)} className='absolute left-6' alt="close-icon" />
-                        <div className='flex items-center justify-center gap-7 font-bold'>
-                            <div>home</div>
-                            <div>shop</div>
-                            <div>about</div>
-                            <div>contact</div>
+                <div className="w-full max-w-[1440px] flex flex-col">
+
+                    <div className='h-[360px] relative'>    
+                        <div className={`${ isOpen ? 'block' : 'hidden'} fixed z-1 w-full h-full bg-blacklowop`}></div>
+                        <img src={heroone} className='absolute' alt="hero-1" />
+                        <div className={`${ isOpen ? 'hidden' : 'flex' } relative z-1 flex items-center justify-center w-full text-white text-[30px] p-5 pt-7`}>
+                            <div>room</div>
+                            <img src={hamburger} className='absolute left-5' onClick={() => setIsOpen(!isOpen)} alt="hamburger-icon" />
+                        </div>
+                        <div className={`${ isOpen ? 'flex' : 'hidden' } relative z-2 flex items-center justify-end bg-white p-6 pt-10 pb-10 w-full gap-8`}>
+                            <img src={close} onClick={() => setIsOpen(!isOpen)} className='absolute left-6' alt="close-icon" />
+                            <div className='flex items-center justify-center gap-7 font-bold'>
+                                <div>home</div>
+                                <div>shop</div>
+                                <div>about</div>
+                                <div>contact</div>
+                            </div>
+                        </div>
+                        <div className='absolute right-0 bottom-0 z-5 bg-black flex items-center'>
+                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px]'><img src={arrowleft} className='w-[10px]' alt="left-arrow-icon" /></button>
+                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px]'><img src={arrowright} className='w-[10px]' alt="right-arrow-icon" /></button>
                         </div>
                     </div>
-                        
+
                     
+                    <div className='p-8 flex flex-col items-start gap-8 mt-7'>
+                        <div className='flex flex-col gap-4 leading-[1.3]'>
+                            <div className='font-semibold tracking-tighter text-[2.55rem] leading-[.9]'>Discover innovative ways to decorate</div>
+                            <div className='text-grey500 text-[15.5px] leading-[1.4]'>We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in brinking your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.</div>
+                        </div>
+
+                        <div className='flex gap-4 items-center'>
+                            <div className='tracking-[.8rem] font-medium text-[15px] '>SHOP NOW </div>
+                            <img src={arrow} alt="arrow-icon" />
+                        </div>    
+                    </div>
+
+                    
+
                 </div>
             </div>
         </>
