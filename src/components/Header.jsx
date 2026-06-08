239677d4dbf69cbc068396137147f78795c6a58a
aiminnano <gabriellesmateo@gmail.com>
2026-06-09 04:46:56 +0800

    finalized the design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import hamburger from '../../images/icon-hamburger.svg'
 import heroone from '../../images/mobile-image-hero-1.jpg'
+import herotwo from '../../images/mobile-image-hero-2.jpg'
+import herothree from '../../images/mobile-image-hero-3.jpg'
 import close from '../../images/icon-close.svg'
 import Arrow from '../../images/icon-arrow.svg?react'
 import arrowleft from '../../images/icon-angle-left.svg'
@@ -11,12 +13,26 @@ import hero3d from '../../images/desktop-image-hero-3.jpg'
 import { useState } from 'react'
 
 function Header(){
-
+    const desktopImages = [hero1d, hero2d, hero3d];
+    const mobileImages = [heroone, herotwo, herothree]
     const [isOpen, setIsOpen] = useState(false);
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState(0);
+    const [fade, setFade] = useState(true)
 
-    function previous() {
-        setCount(currCount => currCount - 1)
+    const prev = () => {
+        setFade(false)
+        setTimeout(() => {
+            setCount(count === 0 ? desktopImages.length - 1 : count - 1);
+            setFade(true)
+        }, 50);  
+    }
+
+    const next = () => {
+        setFade(false);
+        setTimeout(() => {
+            setCount(count === desktopImages.length - 1 ? 0 : count + 1);
+            setFade(true);
+        }, 50);  
     }
 
     return(
@@ -26,8 +42,8 @@ function Header(){
 
                     <div className='h-[360px] relative'>    
                         <div className={`${ isOpen ? 'block' : 'hidden'} fixed z-1 w-full h-full bg-blacklowop`}></div>
-                        <img src={heroone} className='absolute lg:hidden w-full h-full object-cover' alt="hero-1" />
-                        <img src={hero1d} className='hidden lg:block w-full h-[534px] object-cover ' alt="desktop-img" />
+                        <img src={mobileImages[count]} className={`transition-opacity duration-50 ${fade ? 'opacity-100' : 'opacity-0'} absolute lg:hidden w-full h-full object-cover`} alt="mobile-img" />
+                        <img src={desktopImages[count]} className={`transition-opacity duration-50 ${fade ? 'opacity-100' : 'opacity-0'} hidden lg:block w-full h-[534px] object-cover`} alt="desktop-img" />
                         <div className='lg:hidden'>
                             <div className={`${ isOpen ? 'hidden' : 'flex' } relative z-1 flex items-center justify-center w-full text-white text-[30px] p-5 pt-7`}>
                                 <div>room</div>
@@ -53,8 +69,8 @@ function Header(){
                             </div>
                         </div>
                         <div className='absolute right-0 bottom-0 z-5 bg-black flex items-center lg:hidden'>
-                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px]'><img src={arrowleft} className='w-[10px]' alt="left-arrow-icon" /></button>
-                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px]'><img src={arrowright} className='w-[10px]' alt="right-arrow-icon" /></button>
+                            <button type="button" onClick={prev} className='flex items-center justify-center w-[56px] h-[55px] '><img src={arrowleft} className='w-[10px]' alt="left-arrow-icon" /></button>
+                            <button type="button" onClick={next} className='flex items-center justify-center w-[56px] h-[55px] '><img src={arrowright} className='w-[10px]' alt="right-arrow-icon" /></button>
                         </div>
                     </div>
 
@@ -70,8 +86,8 @@ function Header(){
                             <Arrow className='text-black group-hover:text-grey500' />
                         </div>
                         <div className='hidden lg:flex absolute left-0 bottom-0 z-5 bg-black flex items-center '>
-                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px] lg:h-[78px] lg:w-[82px] hover:bg-grey800 cursor-pointer'><img src={arrowleft} className='w-[10px] lg:w-[14px]' alt="left-arrow-icon" /></button>
-                            <button type="button" className='flex items-center justify-center w-[56px] h-[55px] lg:h-[78px] lg:w-[82px] hover:bg-grey800 cursor-pointer'><img src={arrowright} className='w-[10px] lg:w-[14px]' alt="right-arrow-icon" /></button>
+                            <button type="button" onClick={prev} className='flex items-center justify-center w-[56px] h-[55px] lg:h-[78px] lg:w-[82px] hover:bg-grey800 cursor-pointer'><img src={arrowleft} className='w-[10px] lg:w-[14px]' alt="left-arrow-icon" /></button>
+                            <button type="button" onClick={next} className='flex items-center justify-center w-[56px] h-[55px] lg:h-[78px] lg:w-[82px] hover:bg-grey800 cursor-pointer'><img src={arrowright} className='w-[10px] lg:w-[14px]' alt="right-arrow-icon" /></button>
                         </div>    
                     </div>
 
